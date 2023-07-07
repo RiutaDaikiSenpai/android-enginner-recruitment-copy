503a8cf4473ca2a2eb7645e172f3028cb0a36f10
RiutaDaikiSenpai <daikiriuta0926@gmail.com>
2023-07-08 05:56:36 +0900

    ヘッダーを実装

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/RecipeListScreen.kt
@@ -6,13 +6,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +22,6 @@ import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.oishikenko.android.recruitment.feature.R
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalLifecycleComposeApi::class)
 
 @Composable
 fun RecipePageScreen() {
@@ -54,19 +53,6 @@ fun ListScreen(
     }
 }
 
-//@Composable
-//fun HeaderScreen() {
-//    TopAppBar(
-//        modifier = Modifier,
-//        backgroundColor = Color.White,
-//        elevation = 0.dp
-//    ) {
-//        HorizontalAlignment = Alignment.CenterHorizontally
-//        Text(text = stringResource(id = R.string.cooking_records_title), fontSize = 20 .sp)
-//    }
-//}
-
-
 
 @Composable
 fun HeaderScreen() {
@@ -78,28 +64,40 @@ fun HeaderScreen() {
     TopAppBar(
         backgroundColor = Color.Transparent,
         elevation = 0.dp,
-        modifier= Modifier.fillMaxWidth()) {
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val imageModifier = Modifier
+            .size(60.dp)
 
-        //TopAppBar Content
-        Box(Modifier.height(32.dp)) {
+        val textModifier = Modifier
+            .padding(0.dp, 0.dp, 10.dp, 0.dp)
 
-            //Title
-            Row(Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically) {
+        Box(
+            Modifier
+                .height(64.dp)
+                .fillMaxSize()
+        ) {
 
-                ProvideTextStyle(value = MaterialTheme.typography.h6) {
-                    CompositionLocalProvider(
-                        LocalContentAlpha provides ContentAlpha.high,
-                    ){
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Center,
-                            maxLines = 1,
-                            text = stringResource(id = R.string.cooking_records_title)
-                        )
-                        Image(painter = painterResource(id = R.drawable.group), contentDescription = "")
-                    }
-                }
+            Row(
+                Modifier
+                    .fillMaxSize()
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    maxLines = 1,
+                    text = stringResource(id = R.string.cooking_records_title),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = textModifier
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.group),
+                    contentDescription = "料理",
+                    modifier = imageModifier
+                )
             }
         }
     }
